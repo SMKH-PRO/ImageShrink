523959dba7edfa04b3d117eec5ac72d742035249
KASHAN HAIDER <smkh.kashan@gmAIL.COM>
2020-08-11 06:30:47 +0500

    finalizing stuff

--- main.js
+++ main.js
@@ -8,37 +8,64 @@ const {app, BrowserWindow, Menu, ipcMain} = require("electron")
 process.env.NODE_ENV = "dev"
 
 const isMac = process.platform == 'darwin'
-const isDev = process.env.NODE_ENV == "production"
+const isDev = process.env.NODE_ENV !== "production"
 
 
 let win;
 let settingsWindow;
+let aboutWindow;
+const createAboutWindow = () => {
 
+    if (! aboutWindow) { // If not already opened
+        aboutWindow = new BrowserWindow({
+            width: 400,
+            height: 350,
+            webPreferences: {
+                nodeIntegration: true
+            },
+            resizable: false
+
+        })
+        aboutWindow.loadURL(url.format({
+            pathname: path.join(__dirname, "./src/about.html"),
+            protocol: 'file',
+            slashes: true
+        }))
+        aboutWindow.on("closed", () => {
+            aboutWindow = null;
+        })
+        // settingsWindow.webContents.openDevTools()
+
+    } else { // Handle Behaviour When Opening again from the menu.
+        console.log("Dont Open another instance of about window.")
+
+        aboutWindow.restore()
+
+    }
+}
 const createSettingsWindow = () => {
 
-    if (!settingsWindow) {// If not already opened
-              settingsWindow = new BrowserWindow({
-                  width: 700,
-                  height: 500,
-                  webPreferences: {
-                      nodeIntegration: true
-                  }
-                  
-              })
-              settingsWindow.loadURL(url.format({
-                  pathname: path.join(__dirname, "./src/setting.html"),
-                  protocol: 'file',
-                  slashes: true,
-                  resizable: !isDev
-              }))
-              settingsWindow.on("closed", () => {
-                settingsWindow = null;
-              })
-              //settingsWindow.webContents.openDevTools()
+    if (! settingsWindow) { // If not already opened
+        settingsWindow = new BrowserWindow({
+            width: 700,
+            height: 500,
+            webPreferences: {
+                nodeIntegration: true
+            },
+            resizable: ! isDev
 
-    } 
-    else {
-        //Handle Behaviour When Opening again from the menu.
+        })
+        settingsWindow.loadURL(url.format({
+            pathname: path.join(__dirname, "./src/setting.html"),
+            protocol: 'file',
+            slashes: true
+        }))
+        settingsWindow.on("closed", () => {
+            settingsWindow = null;
+        })
+        // settingsWindow.webContents.openDevTools()
+
+    } else { // Handle Behaviour When Opening again from the menu.
         console.log("Dont Open another instance of about window.")
 
         settingsWindow.restore()
@@ -68,7 +95,7 @@ const createWindow = () => {
     // win.webContents.openDevTools()
 }
 app.on('ready', createWindow)
-ipcMain.on('openSettings', ()=>createSettingsWindow())
+ipcMain.on('openSettings', () => createSettingsWindow())
 ipcMain.on('image:compress', (e, filesPath) => {
     // console.log("event recievedx")
     // console.log("ReceivedEVENT=> ",filesPath)
@@ -77,9 +104,7 @@ ipcMain.on('image:compress', (e, filesPath) => {
     // console.log("quality=> ",CompressQuality)
 
     win.webContents.send("Compress:Started", true)
-    ImageShrink(filesPath)
-      .then(r => win.webContents.send("Compress:Completed", r))
-      .catch(e=>win.webContents.send("Compress:Error", e))
+    ImageShrink(filesPath).then(r => win.webContents.send("Compress:Completed", r)).catch(e => win.webContents.send("Compress:Error", e))
 })
 app.on('window-all-closed', () => {
     if (! isMac) {
@@ -95,11 +120,28 @@ app.on("activate", () => {
 
 
 const menu = [
-    ... isMac ? [
+    
+    ...(isMac ? [
         {
-            role: 'appMenu'
+            label: app.name,
+            submenu: [
+                {
+                    label: `About ${app.name}`,
+                    click: createAboutWindow
+                },
+                { type: 'separator' },
+                { role: 'hide' },
+                { role: 'hideothers' },
+                { role: 'unhide' },
+                { type: 'separator' },
+                { role: 'quit' }
+
+            ]
+    
         }
-    ] : [], {
+    ] : []), 
+    
+     {
         label: 'File',
         submenu: [
             {
@@ -113,27 +155,39 @@ const menu = [
         ]
     },
 
-    // ... isDev ? [
-        {
-            label: "Developer",
+    ...(!isMac ? [{
+            label: 'Help',
             submenu: [
                 {
-                    role: 'reload'
-                },
-                {
-                    role: 'forcereload'
-                },
-                {
-                    role: 'reload'
-                },
-                {
-                    type: 'separator'
-                }, {
-                    role: 'toggledevtools'
-                },
-
-
+                    label: 'About',
+                    click: createAboutWindow
+                }
             ]
-        }
-    // ] : []
+
+        }] :[]),
+
+    ...(isDev ? [
+           {
+        label: "Developer",
+        submenu: [
+            {
+                role: 'reload'
+            },
+            {
+                role: 'forcereload'
+            },
+            {
+                role: 'reload'
+            },
+            {
+                type: 'separator'
+            }, {
+                role: 'toggledevtools'
+            },
+
+
+        ]
+    }
+    ] : [])
 ]
+
